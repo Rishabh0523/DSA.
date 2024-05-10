1773efda8b2f5bbc6affe26072d94891414d9cb3
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-10 13:28:26 +0530

    Add files via upload

---
+++ Introduction To C++/Switch Statement/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int day = 3;
+
+switch(day){
+    case 1:
+    cout<<"Monday"<<endl;
+    break;
+
+    case 2:
+    cout<<"Tuesday"<<endl;
+    break;
+
+    case 3 :
+    cout<<"Wed"<<endl;
+    break;
+
+    default:
+    cout<<"Nothing"<<endl;
+}
+
+
+
+    return 0;z
+}
