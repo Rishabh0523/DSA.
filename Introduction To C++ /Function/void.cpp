d7dfd1c4de033bd906e83dd85db849823fd986d6
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-15 23:15:12 +0530

    Add files via upload

---
+++ Introduction To C++ /Function/void.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printName(){
+    cout<<"Rishabh"<<endl;
+}
+
+int main(){
+
+printName();
+
+
+return 0;
+
+}
+
---
+++ Introduction To C++ /Operators/Assigment/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+    
+int arr=5;
+cout<<arr<<endl;
+
+// arr= arr + 10;
+// arr += 10;
+
+
+arr *=10;
+
+cout<<arr<<endl;
+
+ 
+}
