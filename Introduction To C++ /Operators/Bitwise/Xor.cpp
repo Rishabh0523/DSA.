f0a6bb97e94692c6476d20e82ae6b6e38850288d
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-18 23:32:35 +0530

    Update Xor.cpp

--- Introduction To C++ /Operators/Bitwise/Xor.cpp
+++ Introduction To C++ /Operators/Bitwise/Xor.cpp
@@ -4,8 +4,8 @@ int main()
 
 {
   
-cout<<(5^5)<<endl; // We will get 0;
-cout<<(5^4)<<endl; // We will get 1;
+cout<<(5^5)<<endl; // We will get 0 here;
+cout<<(5^4)<<endl; // We will get 1 here;
 
     return 0;
 }
