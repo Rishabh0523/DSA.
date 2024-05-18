2f413651b29a82c0eced3b02ec28d1d9acb67df0
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2024-05-19 00:22:48 +0530

    Add files via upload

---
+++ Introduction To C++/Introduction To C++ /Operators/Logical/AND.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+    
+// bool cond1 = true;
+// bool cond2 = true;
+// bool cond3 = false;
+
+bool cond1 = (10>5);
+bool cond2 = (2==2);
+bool cond3 = (6<9);
+
+if(cond1 && cond2 && cond3){
+    cout<<"All Conditions are True"<<endl;
+}
+
+else{
+    cout<<"All Conditions are not True"<<endl;
+}
+
+ 
+}
---
+++ Introduction To C++/Introduction To C++ /Operators/Unary/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+
+
+{
+    
+    // int a=5;
+    // cout<<a++<<endl;
+    // cout<<a;
+    
+    //  int a=5;
+    // cout<<++a<<endl;
+    // cout<<a;
+    
+    //  int a=5;
+    // cout<<a--<<endl;
+    // cout<<a;
+    
+     int a=5;
+    cout<<--a<<endl;
+    cout<<a;
+
+}
